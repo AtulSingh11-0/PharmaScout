11d018a505a415f6cdc2c3b4f0c9361fd587445f
Atul Singh <cocatul11@gmail.com>
2024-08-01 23:49:22 +0530

    chore: Add initial project files and configuration

---
+++ src/test/java/com/demo/pharmascout/PharmaScoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.pharmascout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmaScoutApplicationTests {
+
+	@Test
+	void contextLoads () {
+	}
+
+}
