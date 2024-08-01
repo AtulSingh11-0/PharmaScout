e7035af42437f1cbe120c06487d5d1541b069473
Atul Singh <cocatul11@gmail.com>
2024-08-01 23:50:18 +0530

    feat: Add REST API endpoints for managing medicine entries

---
+++ src/main/java/com/demo/pharmascout/medicine/entry/controller/MedicineController.java
@@ -0,0 +1,73 @@
+package com.demo.pharmascout.medicine.entry.controller;
+
+import com.demo.pharmascout.medicine.entry.model.MedicineModel;
+import com.demo.pharmascout.medicine.entry.service.MedicineService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/medicine")
+public class MedicineController {
+
+	private final MedicineService medicineService;
+
+	public MedicineController(MedicineService medicineService) {
+		this.medicineService = medicineService;
+	}
+
+	@PostMapping("/")
+	public ResponseEntity<?> handleAddMedicine (
+			@RequestBody MedicineModel medicineModel
+	) {
+		try {
+			return ResponseEntity.status(HttpStatus.CREATED).body(medicineService.addMedicine(medicineModel));
+		} catch ( Exception e ) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<?> handleUpdateMedicine (
+			@RequestBody MedicineModel medicineModel,
+			@PathVariable int id
+	) {
+		try {
+			return ResponseEntity.ok(medicineService.updateMedicine(medicineModel, id));
+		} catch ( Exception e ) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<?> handleGetMedicineById (
+			@PathVariable int id
+	) {
+		try {
+			return ResponseEntity.ok(medicineService.getMedicineById(id));
+		} catch ( Exception e ) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> handleDeleteMedicineById (
+			@PathVariable int id
+	) {
+		try {
+			medicineService.deleteMedicineById(id);
+			return ResponseEntity.ok("Medicine deleted successfully");
+		} catch ( Exception e ) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
+
+	@GetMapping("/")
+	public ResponseEntity<?> handleGetAllMedicines () {
+		try {
+			return ResponseEntity.ok(medicineService.getAllMedicines());
+		} catch ( Exception e ) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
+}
