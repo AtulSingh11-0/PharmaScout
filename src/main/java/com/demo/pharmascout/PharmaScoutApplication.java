21ba1d130abe5c466d05718ddd59f96d37d3797f
Atul Singh <cocatul11@gmail.com>
2024-08-02 23:34:01 +0530

    feat: Import ExpiredMedicineService for managing expired medicine entries

--- src/main/java/com/demo/pharmascout/PharmaScoutApplication.java
+++ src/main/java/com/demo/pharmascout/PharmaScoutApplication.java
@@ -1,5 +1,6 @@
 package com.demo.pharmascout;
 
+import com.demo.pharmascout.medicine.expired.service.ExpiredMedicineService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
