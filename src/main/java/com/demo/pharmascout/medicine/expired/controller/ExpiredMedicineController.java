1b885992756c3693d9e32e0d27cfe8b642d7df4a
Atul Singh <cocatul11@gmail.com>
2024-08-02 23:34:38 +0530

    feat: Add ExpiredMedicineController for handling expired medicine requests

---
+++ src/main/java/com/demo/pharmascout/medicine/expired/controller/ExpiredMedicineController.java
@@ -0,0 +1,29 @@
+package com.demo.pharmascout.medicine.expired.controller;
+
+import com.demo.pharmascout.medicine.expired.model.ExpiredMedicineModel;
+import com.demo.pharmascout.medicine.expired.service.ExpiredMedicineService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/expired-medicines")
+public class ExpiredMedicineController {
+
+	private final ExpiredMedicineService expiredMedicineService;
+
+	public ExpiredMedicineController(ExpiredMedicineService expiredMedicineService) {
+		this.expiredMedicineService = expiredMedicineService;
+	}
+
+	@GetMapping("/{date}")
+	public ResponseEntity< List < ExpiredMedicineModel > > handleGetAllExpiredMedicines(
+			@PathVariable String date
+	) {
+		return ResponseEntity.ok(expiredMedicineService.getAllExpiredMedicines(date));
+	}
+}
