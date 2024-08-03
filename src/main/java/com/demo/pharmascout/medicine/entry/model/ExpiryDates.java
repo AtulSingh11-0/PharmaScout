35b00bbf2e4c66e11a34a9f196f22b675d6446af
Atul Singh <cocatul11@gmail.com>
2024-08-03 22:35:29 +0530

    feat: Add ExpiryDates entity for managing medicine expiry dates

---
+++ src/main/java/com/demo/pharmascout/medicine/entry/model/ExpiryDates.java
@@ -0,0 +1,36 @@
+package com.demo.pharmascout.medicine.entry.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "expiry_dates")
+@ToString(exclude = "medicine")
+@EqualsAndHashCode(of = {"expiryDate"})
+public class ExpiryDates {
+
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@JsonFormat (pattern = "yyyy-MM-dd", timezone = "Asia/Kolkata", shape = JsonFormat.Shape.STRING, locale = "en_IN")
+	private LocalDate expiryDate;
+
+	@Column (nullable = false)
+	private int quantity;
+
+	@JsonBackReference
+	@ManyToOne (fetch = FetchType.LAZY, optional = false)
+	@JoinColumn (name = "medicine_id", referencedColumnName = "id")
+	private MedicineModel medicine;
+
+}
