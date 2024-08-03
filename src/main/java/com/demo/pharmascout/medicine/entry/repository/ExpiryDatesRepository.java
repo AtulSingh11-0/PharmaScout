cf67a40cb590b47745bba61a3f1b0e3c90d6f64f
Atul Singh <cocatul11@gmail.com>
2024-08-03 22:35:40 +0530

    feat: Add ExpiryDatesRepository for managing medicine expiry dates

---
+++ src/main/java/com/demo/pharmascout/medicine/entry/repository/ExpiryDatesRepository.java
@@ -0,0 +1,11 @@
+package com.demo.pharmascout.medicine.entry.repository;
+
+import com.demo.pharmascout.medicine.entry.model.ExpiryDates;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface ExpiryDatesRepository extends JpaRepository < ExpiryDates, LocalDate > {
+	List< ExpiryDates > findAllByExpiryDateBefore ( LocalDate date );
+}
