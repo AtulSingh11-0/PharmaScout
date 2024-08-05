8276e2f1995590f03e07126b2e280ef3826ca3a0
Atul Singh <cocatul11@gmail.com>
2024-08-05 21:07:56 +0530

    Refactor MedicineService to add method for updating medicine quantity

--- src/main/java/com/demo/pharmascout/medicine/entry/service/MedicineService.java
+++ src/main/java/com/demo/pharmascout/medicine/entry/service/MedicineService.java
@@ -5,8 +5,9 @@ import com.demo.pharmascout.medicine.entry.model.MedicineModel;
 import java.util.List;
 
 public interface MedicineService {
-	public MedicineModel getMedicineById ( Long id );
 	public List< MedicineModel > getAllMedicines();
+	public MedicineModel getMedicineById ( Long id );
 	public MedicineModel addMedicine ( MedicineModel medicineModel );
+	public void updateMedicineQuantity (MedicineModel medicineModel, int quantity );
 	public MedicineModel updateMedicine ( MedicineModel medicineModel, MedicineModel medicineToUpdate );
 }
