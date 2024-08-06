e9604f991c1b2677d5468df8a9ef4ebee72d4f43
Atul Singh <cocatul11@gmail.com>
2024-08-06 20:55:27 +0530

    feat: Add TrendingMedicineService interface for trending medicine functionality

---
+++ src/main/java/com/demo/pharmascout/medicine/search/service/TrendingMedicineService.java
@@ -0,0 +1,12 @@
+package com.demo.pharmascout.medicine.search.service;
+
+import com.demo.pharmascout.medicine.entry.model.MedicineModel;
+import com.demo.pharmascout.medicine.search.model.TrendingMedicine;
+
+import java.time.LocalDate;
+
+public interface TrendingMedicineService {
+	public MedicineModel getTrendingMedicine ( LocalDate date, String name, int quantity );
+
+	public void addTrendingMedicine ( TrendingMedicine trendingMedicine );
+}
