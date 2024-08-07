09c0dbe4162aba87c876ed764909f86313c27da9
Atul Singh <cocatul11@gmail.com>
2024-08-07 23:46:42 +0530

    chore: Refactor ExpiryDatesServiceImpl to use transactional updates and delete expired dates

--- src/main/java/com/demo/pharmascout/medicine/entry/service/impl/ExpiryDatesServiceImpl.java
+++ src/main/java/com/demo/pharmascout/medicine/entry/service/impl/ExpiryDatesServiceImpl.java
@@ -2,12 +2,12 @@ package com.demo.pharmascout.medicine.entry.service.impl;
 
 import com.demo.pharmascout.medicine.entry.model.ExpiryDates;
 import com.demo.pharmascout.medicine.entry.repository.ExpiryDatesRepository;
-import com.demo.pharmascout.medicine.entry.repository.MedicineRepository;
 import com.demo.pharmascout.medicine.entry.service.ExpiryDatesService;
 import com.demo.pharmascout.medicine.entry.service.MedicineService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -17,13 +17,14 @@ import java.util.List;
 public class ExpiryDatesServiceImpl implements ExpiryDatesService {
 
 	private final MedicineService medicineService;
-	private final MedicineRepository medicineRepository;
 	private final ExpiryDatesRepository expiryDatesRepository;
 
 	@Override
+	@Transactional
 	public void setMedicinesAsExpired ( List< ExpiryDates > expiryDates ) {
 		try {
 			for ( ExpiryDates expiryDate : expiryDates ) { // iterate through the expiry dates
+				expiryDate.setExpired(true); // set the expiry date as expired
 				updateExpiryDates(expiryDate); // update the expiry dates
 				medicineService.updateMedicineQuantity(expiryDate.getMedicine(), expiryDate.getQuantity()); // update the medicine quantity
 			}
@@ -36,7 +37,9 @@ public class ExpiryDatesServiceImpl implements ExpiryDatesService {
 	@Override
 	public void updateExpiryDates ( ExpiryDates expiryDates ) {
 		try {
-			expiryDates.setExpired(true); // set the expiry date as expired
+			if ( expiryDates.getQuantity() == 0 || expiryDates.isExpired() ) {
+				expiryDatesRepository.delete(expiryDates); // delete the expiry date
+			}
 			expiryDatesRepository.save(expiryDates); // save the expiry date
 		} catch ( Exception e ) {
 			log.error("Error occurred while updating expiry dates: {}", e.getMessage());
