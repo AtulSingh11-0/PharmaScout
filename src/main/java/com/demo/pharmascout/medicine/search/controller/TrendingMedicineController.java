68f7a921cd95e94c53760f74728d4477fd85babc
Atul Singh <cocatul11@gmail.com>
2024-08-07 10:32:09 +0530

    chore: Update TrendingMedicineController to return a list of trending medicines

--- src/main/java/com/demo/pharmascout/medicine/search/controller/TrendingMedicineController.java
+++ src/main/java/com/demo/pharmascout/medicine/search/controller/TrendingMedicineController.java
@@ -3,11 +3,14 @@ package com.demo.pharmascout.medicine.search.controller;
 import com.demo.pharmascout.medicine.entry.model.MedicineModel;
 import com.demo.pharmascout.medicine.search.service.TrendingMedicineService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping ( "/api/v1/trending-medicine" )
@@ -22,12 +25,8 @@ public class TrendingMedicineController {
 			@RequestParam ( value = "quantity", required = true ) int quantity
 	) {
 		try {
-			MedicineModel trendingMedicine = service.getTrendingMedicine(LocalDate.now(), name, quantity);
-			if ( trendingMedicine == null ) {
-				return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No medicine found");
-			} else {
-				return ResponseEntity.ok(trendingMedicine);
-			}
+			List< MedicineModel > trendingMedicine = service.getTrendingMedicine(LocalDate.now(), name, quantity);
+			return ResponseEntity.ok(trendingMedicine);
 		} catch ( Exception e ) {
 			return ResponseEntity.badRequest().body(e.getMessage());
 		}
