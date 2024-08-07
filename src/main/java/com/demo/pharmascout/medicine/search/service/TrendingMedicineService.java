d7d291409bd42c5f73bed9e861c8a671332f223b
Atul Singh <cocatul11@gmail.com>
2024-08-07 23:46:56 +0530

    chore: Update TrendingMedicineService to return a single trending medicine

--- src/main/java/com/demo/pharmascout/medicine/search/service/TrendingMedicineService.java
+++ src/main/java/com/demo/pharmascout/medicine/search/service/TrendingMedicineService.java
@@ -4,9 +4,8 @@ import com.demo.pharmascout.medicine.entry.model.MedicineModel;
 import com.demo.pharmascout.medicine.search.model.TrendingMedicine;
 
 import java.time.LocalDate;
-import java.util.List;
 
 public interface TrendingMedicineService {
-	public List< MedicineModel > getTrendingMedicine ( LocalDate date, String name, int quantity);
+	public MedicineModel getTrendingMedicine ( LocalDate date, String name, int quantity);
 	public void addTrendingMedicine ( TrendingMedicine trendingMedicine );
 }
