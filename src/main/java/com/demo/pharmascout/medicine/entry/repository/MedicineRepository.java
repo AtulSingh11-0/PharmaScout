b5c5a9127cb0ffed0c874c4bc8df409d6beef477
Atul Singh <cocatul11@gmail.com>
2024-08-07 23:46:36 +0530

    chore: Update MedicineRepository to use findAllByName instead of findByGenericName

--- src/main/java/com/demo/pharmascout/medicine/entry/repository/MedicineRepository.java
+++ src/main/java/com/demo/pharmascout/medicine/entry/repository/MedicineRepository.java
@@ -4,11 +4,10 @@ import com.demo.pharmascout.medicine.entry.model.MedicineModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface MedicineRepository extends JpaRepository< MedicineModel, Long > {
 	Optional< MedicineModel > findByName ( String name );
-	Optional< List< MedicineModel > > findAllByGenericName ( String genericName );
+	Optional< MedicineModel > findAllByName ( String genericName );
 }
