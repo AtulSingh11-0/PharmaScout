93e5f96503cc6544329a39b3ef3dfc3dbba77d4d
Atul Singh <cocatul11@gmail.com>
2024-08-08 21:36:33 +0530

    chore: Update ExpiryDatesServiceImpl to optimize expiry date management

--- src/main/java/com/demo/pharmascout/medicine/entry/service/impl/ExpiryDatesServiceImpl.java
+++ src/main/java/com/demo/pharmascout/medicine/entry/service/impl/ExpiryDatesServiceImpl.java
@@ -20,13 +20,12 @@ public class ExpiryDatesServiceImpl implements ExpiryDatesService {
 	private final ExpiryDatesRepository expiryDatesRepository;
 
 	@Override
-	@Transactional
 	public void setMedicinesAsExpired ( List< ExpiryDates > expiryDates ) {
 		try {
 			for ( ExpiryDates expiryDate : expiryDates ) { // iterate through the expiry dates
+				medicineService.updateMedicineQuantity(expiryDate.getMedicine(), expiryDate.getQuantity()); // update the medicine quantity
 				expiryDate.setExpired(true); // set the expiry date as expired
 				updateExpiryDates(expiryDate); // update the expiry dates
-				medicineService.updateMedicineQuantity(expiryDate.getMedicine(), expiryDate.getQuantity()); // update the medicine quantity
 			}
 		} catch ( Exception e ) {
 			log.error("Error occurred while setting medicines as expired: {}", e.getMessage());
@@ -35,15 +34,28 @@ public class ExpiryDatesServiceImpl implements ExpiryDatesService {
 	}
 
 	@Override
-	public void updateExpiryDates ( ExpiryDates expiryDates ) {
+	public void updateExpiryDates ( ExpiryDates expiryDate ) {
 		try {
-			if ( expiryDates.getQuantity() == 0 || expiryDates.isExpired() ) {
-				expiryDatesRepository.delete(expiryDates); // delete the expiry date
+			if ( expiryDate.getQuantity() == 0 || expiryDate.isExpired() ) {
+				expiryDatesRepository.delete(expiryDate); // delete the expiry date
+			} else {
+				expiryDatesRepository.save(expiryDate); // save the expiry date
 			}
-			expiryDatesRepository.save(expiryDates); // save the expiry date
 		} catch ( Exception e ) {
 			log.error("Error occurred while updating expiry dates: {}", e.getMessage());
 			throw new RuntimeException("Error occurred while updating expiry dates");
 		}
 	}
+
+	@Transactional
+	@Override
+	public void updateExpiryDatesList(List<ExpiryDates> toDelete, List<ExpiryDates> toUpdate) {
+		if (!toDelete.isEmpty()) {
+			expiryDatesRepository.deleteAll(toDelete); // delete the expired expiry dates
+		}
+		if (!toUpdate.isEmpty()) {
+			expiryDatesRepository.saveAll(toUpdate); // save the updated expiry dates
+		}
+	}
+
 }
