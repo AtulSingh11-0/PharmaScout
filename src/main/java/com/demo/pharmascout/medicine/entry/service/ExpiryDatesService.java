28e1b921e3cda148a9a7484f77631393b5bbbf37
Atul Singh <cocatul11@gmail.com>
2024-08-08 21:36:29 +0530

    chore: Update ExpiryDatesService to include method for updating expiry dates list

--- src/main/java/com/demo/pharmascout/medicine/entry/service/ExpiryDatesService.java
+++ src/main/java/com/demo/pharmascout/medicine/entry/service/ExpiryDatesService.java
@@ -1,11 +1,11 @@
 package com.demo.pharmascout.medicine.entry.service;
 
 import com.demo.pharmascout.medicine.entry.model.ExpiryDates;
-import com.demo.pharmascout.medicine.expired.model.ExpiredMedicineModel;
 
 import java.util.List;
 
 public interface ExpiryDatesService {
 	public void updateExpiryDates( ExpiryDates expiryDates );
 	public void setMedicinesAsExpired ( List< ExpiryDates > expiryDates );
+	public void updateExpiryDatesList(List<ExpiryDates> toDelete, List<ExpiryDates> toUpdate);
 }
